9730d0304fb6238ab9460cc7b3fa65e4a8f6786d
Andi <familie.mayer@aon.at>
2019-03-21 06:28:20 +0100

    ttt

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,48 @@
 import 'package:flutter/material.dart';
-import 'src/app.dart';
 
-main() => runApp(App());
+main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food Tester'];
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('EasyList'),
+        ),
+        body: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                child: Text('Add Product'),
+                onPressed: () {},
+              ),
+            ),
+            Column(
+              children: _products
+                  .map(
+                    (element) => Card(
+                          child: Column(
+                            children: <Widget>[
+                              Image.asset('assets/food.jpg'),
+                              Text('Food Paradise'),
+                            ],
+                          ),
+                        ),
+                  )
+                  .toList(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
